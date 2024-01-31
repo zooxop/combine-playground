ba1eb13989edd49376bd8d4f4d3a36e67c4632a0
Cheolhyun Mun <zooxop@gmail.com>
2024-01-31 14:23:07 +0900

    [Add] Initial project

---
+++ CombinePlayground/CombinePlayground/CombinePlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombinePlaygroundApp.swift
+//  CombinePlayground
+//
+//  Created by 문철현 on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct CombinePlaygroundApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
---
+++ CombinePlayground/CombinePlayground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CombinePlayground
+//
+//  Created by 문철현 on 1/31/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  ContentView()
+}
